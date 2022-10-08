ade3c807ac3f98a837c718295ed91f6ec323d2be
Diego Fujii <android.mxdiego9@gmail.com>
2022-10-08 14:48:37 +0900

    work?

--- flake.nix
+++ flake.nix
@@ -1,48 +1,51 @@
 {
   description = "diegodox's NeoVim config";
 
-  inputs = {
-    core.url = "path:./core";
-    plugin.url = "path:./plugin";
-  };
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+  inputs.flake-utils.url = github:numtide/flake-utils;
+  inputs.neovim-nightly-overlay.url = github:nix-community/neovim-nightly-overlay;
 
-  outputs = {
-    self,
-    core,
-    plugin,
-    ...
-  }: let
-    inherit (core) system pkgs buildNvim;
-    plugins = core.buildPlugList plugin.plugins;
-  in rec {
-    apps.${system} = rec {
-      nvim = {
-        type = "app";
-        program = "${packages."${system}".default}/bin/nvim";
-      };
-      default = nvim;
-    };
+  outputs = { self, nixpkgs, flake-utils, neovim-nightly-overlay, ... }:
+    with flake-utils.lib;
+    eachSystem defaultSystems (system:
 
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = [packages.${system}.neovim-diegodox];
-    };
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        neovim-unwrapped = pkgs.neovim-unwrapped;
+      in
 
-    overlays.default = final: prev: {
-      neovim-diegodox = packages.${system}.neovim-diegodox;
-      neovimPlugins = pkgs.neovimPlugins;
-    };
+      rec{
 
-    packages.${system} = rec {
-      default = neovim-diegodox;
-      neovim-diegodox = buildNvim {
-        customRC = ''
-          colorscheme codedark
-        '';
-        packages.myVimPackage = {
-          start = plugins;
-          opt = [];
+        ## neovim specify config
+        packages.nvim_withconfig = pkgs.writeShellScriptBin
+          "mynvim"
+          "${neovim-unwrapped}/bin/nvim -u config/init.lua";
+
+
+        ## neovim with config
+        packages.mynvim = pkgs.symlinkJoin {
+          name = "mynvim";
+          paths = [ packages.config packages.nvim_withconfig pkgs.neovim-unwrapped ];
         };
-      };
-    };
-  };
+
+
+        ## Pure neovim (-u NONE)
+        packages.nullvim = pkgs.writeShellScriptBin "nullvim"
+          ''
+            ${neovim-unwrapped}/bin/nvim --clean "$@"
+          '';
+
+        ## Derivation which contains neovim config files.
+        ## We can reference this derivation whenever we need config.
+        packages.config = pkgs.stdenvNoCC.mkDerivation {
+          name = "neovim-config-files";
+          src = self + "/config";
+          phases = [ "installPhase" ];
+          installPhase = ''
+            mkdir -p $out
+            cp -r $src $out/config
+          '';
+        };
+      }
+    );
 }
